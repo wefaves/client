926fa848260ed140c9206bf9d2777a3337db94f5
Margheriti <lilrom13@gmail.com>
2017-05-20 16:05:26 +0200

    add firebase configuration

---
+++ src/assets/js/firebaseconfig.js
@@ -0,0 +1,12 @@
+/**
+ * Created by romain on 2017-05-20.
+ */
+var config = {
+  apiKey: "AIzaSyAI2wt6tMs2oAxTI-iBAv-Qe5FUc3MoRV4",
+  authDomain: "wefaves-prod.firebaseapp.com",
+  databaseURL: "https://wefaves-prod.firebaseio.com",
+  projectId: "wefaves-prod",
+  storageBucket: "wefaves-prod.appspot.com",
+  messagingSenderId: "818124736472"
+};
+firebase.initializeApp(config);

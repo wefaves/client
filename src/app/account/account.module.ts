bf8b09a30382a801960b173cb6c0c27945467e8d
Margheriti <lilrom13@gmail.com>
2017-06-11 23:36:36 +0200

    module

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -8,12 +8,14 @@ import { RegistrationComponent } from './registration/registration.component';
 import { FormsModule } from "@angular/forms";
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { HistoryComponent } from './history/history.component';
+import { FacebookModule } from "ngx-facebook/dist/esm";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    AccountRoutingModule
+    AccountRoutingModule,
+    FacebookModule.forRoot()
   ],
   declarations: [AccountComponent, ProfileComponent, LoginComponent, RegistrationComponent, ProfileComponent, BookmarksComponent, HistoryComponent]
 })

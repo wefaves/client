b4bd79e56a8ddb3f16a67768b477456827c557d4
Margheriti <lilrom13@gmail.com>
2017-07-04 20:41:23 +0200

    edit ci

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# deploy - This script deploy the web application on the right target
+
+
+if [ "$1" == "production" ];
+then
+  firebase use prod;
+  firebase deploy --token $FIREBASE_TOKEN;
+fi
+if [ "$1" == "develop" ];
+then
+  echo tototo
+  ls -l;
+  firebase use dev;
+  firebase deploy --token $FIREBASE_TOKEN;
+fi

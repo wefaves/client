e05a9f23569e934d7cee09e5fa603410fa37d3c9
Margheriti <lilrom13@gmail.com>
2017-07-04 19:08:08 +0200

    ci test

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# deploy - This script deploy the web application on the right target
+
+
+if [ "$1" == "master" ];
+then
+  firebase use prod;
+  firebase deploy --token $FIREBASE_TOKEN;
+fi
+if [ "$1" == "develop" ];
+then
+  firebase use dev;
+  firebase deploy --token $FIREBASE_TOKEN;
+fi

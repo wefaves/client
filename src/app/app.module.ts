d7aa4a80f82bc6434af6a09930f871943987b653
Margheriti <lilrom13@gmail.com>
2017-07-30 20:57:20 +0200

    Add new UI lib (clarity) and update Angular 4.0.0 to 4.3.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AccountModule } from "./account/account.module";
-import { AlertService } from "./services/alert.service";
-import { AuthGuard } from "./_guard/auth.guard";
-import { AuthenticationService } from "./services/authentification.service";
-import { AlertComponent } from "./_directives/alert.component";
+import { AccountModule } from './account/account.module';
+import { AlertService } from './services/alert.service';
+import { AuthGuard } from './_guard/auth.guard';
+import { AuthenticationService } from './services/authentification.service';
+import { AlertComponent } from './_directives/alert.component';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from "./_directives/menu.component";
-import { UserService } from "./services/user.service";
-import { BookmarkService } from "./services/bookmark.service";
-import { HistoryService } from "./services/history.service";
-import { HashLocationStrategy, LocationStrategy } from "@angular/common";
-import { ApiService } from "./services/api.service";
+import { MenuComponent } from './_directives/menu.component';
+import { UserService } from './services/user.service';
+import { BookmarkService } from './services/bookmark.service';
+import { HistoryService } from './services/history.service';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { ApiService } from './services/api.service';
 import { AppComponent } from './app.component';
+import { ClarityModule } from 'clarity-angular';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ClarityModule.forRoot(),
     AccountModule,
     AppRoutingModule
   ],

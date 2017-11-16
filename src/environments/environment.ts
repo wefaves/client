6cf9e1a9c0e5d2ea09a8fad0646551a75b35f522
Margheriti <romain.margheriti@epitech.eu>
2017-11-16 13:40:45 +0100

    share with facebook

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,5 +10,6 @@ export const environment = {
   client_id: '1_35ndt9udie808o84gsgk4ks80k4cgosss8owcow0gwwwwkk0o0',
   client_secret: '360vx1jrejggk8gs4ksg44840kosw4cc4wo84gksow80k0wcgg',
   grant_type: 'password',
-  version: '1.3.2'
+  version: '1.3.2',
+  facebook_app_id: '113512835974670'
 };
